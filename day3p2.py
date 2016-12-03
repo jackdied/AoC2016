84e37bcdddd9ce30f02f1ccbe5466669fc2e621a
Jack Diederich <jackdied@gmail.com>
2016-12-03 12:13:48 -0500

    Done Day3 Part2

--- day3p2.py
+++ day3p2.py
@@ -1,5 +1,7 @@
 import day3
+import mock
 
+'''
 --- Part Two ---
 
 Now that you've helpfully marked up their design documents, it occurs to you that triangles are specified in groups of three vertically. Each set of three numbers in a column specifies a triangle. Rows are unrelated.
@@ -15,3 +17,17 @@ For example, given the following specification, numbers with the same hundreds d
 In your puzzle input, and instead reading by columns, how many of the listed triangles are possible?
 
 '''
+
+def main():
+    new_triples = []
+    cols = zip(*day3.triples)
+    for col in cols:
+        col = list(col)
+        while col:
+            trip = (col.pop(0), col.pop(0), col.pop(0))
+            new_triples.append(trip)
+    with mock.patch.object(day3, 'triples', new_triples):
+        day3.main()
+
+if __name__ == '__main__':
+    main()
