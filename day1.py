4ace31af0443e036d07549210b1de50ee11625d2
Jack Diederich <jackdied@gmail.com>
2016-12-03 12:14:17 -0500

    Left, Right Xignore

--- day1.py
+++ day1.py
@@ -24,11 +24,12 @@ How many blocks away is Easter Bunny HQ?
 my_input = 'L3, R2, L5, R1, L1, L2, L2, R1, R5, R1, L1, L2, R2, R4, L4, L3, L3, R5, L1, R3, L5, L2, R4, L5, R4, R2, L2, L1, R1, L3, L3, R2, R1, L4, L1, L1, R4, R5, R1, L2, L1, R188, R4, L3, R54, L4, R4, R74, R2, L4, R185, R1, R3, R5, L2, L3, R1, L1, L3, R3, R2, L3, L4, R1, L3, L5, L2, R2, L1, R2, R1, L4, R5, R4, L5, L5, L4, R5, R4, L5, L3, R4, R1, L5, L4, L3, R5, L5, L2, L4, R4, R4, R2, L1, L3, L2, R5, R4, L5, R1, R2, R5, L2, R4, R5, L2, L3, R3, L4, R3, L2, R1, R4, L5, R1, L5, L3, R4, L2, L2, L5, L5, R5, R2, L5, R1, L3, L2, L2, R3, L3, L4, R2, R3, L1, R2, L5, L3, R4, L4, R4, R3, L3, R1, L3, R5, L5, R1, R5, R3, L1'
 
 def turn(start_cardinal, leftright):
-    assert leftright in ('L', 'R')
+    assert leftright in ('L', 'R', 'X')
     if leftright == 'L':
         return {'N': 'W', 'W': 'S', 'S': 'E', 'E': 'N'}[start_cardinal]
-    else:
+    elif leftright == 'R':
         return {'N': 'E', 'E': 'S', 'S': 'W', 'W': 'N'}[start_cardinal]
+    return start_cardinal
 
 def step(start_pos, start_cardinal, step_count):
     inc = {'N': (0, 1), 'E': (1, 0), 'S': (0, -1), 'W': (-1, 0)}
