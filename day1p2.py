3de2d4b3a502d31974ad56a47ef2b6e7a35f98b8
Jack Diederich <jackdied@gmail.com>
2016-12-03 11:53:54 -0500

    Day1, Part2

--- day1p2.py
+++ day1p2.py
@@ -9,3 +9,28 @@ For example, if your instructions are R8, R4, R4, R8, the first location you vis
 
 How many blocks away is the first location you visit twice?
 '''
+
+def expand_instructions(instructions):
+    step_by_step = []
+    for instr in instructions:
+        do_turn, do_steps = instr[0], int(instr[1:])
+        step_by_step.append(do_turn+'1')
+        step_by_step.extend(['X1'] * (do_steps-1))
+    return step_by_step
+
+assert ['R1', 'X1'] == expand_instructions(['R2'])
+
+def main():
+    instructions = [instr.strip() for instr in day1.my_input.split(',')]
+    instructions = expand_instructions(instructions)
+
+    here = ((0, 0), 'N')
+    visited = set([here[0]])
+    for instr in instructions:
+        here = day1.move(here, instr)
+        assert here[0] not in visited, here[0]
+        visited.add(here[0])
+
+if __name__ == '__main__':
+    main()
+
