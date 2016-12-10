d6c7ed946a367275f64bf27c8e087b3ee054e165
Jack Diederich <jackdied@gmail.com>
2016-12-10 17:10:48 -0500

    Much cleaner! The way I wanted to do it about halfway through the first time; but again I was going for a timed finish the first time.

--- day8.py
+++ day8.py
@@ -8,65 +8,43 @@ import re
 
 WIDTH = 50
 HEIGHT = 6
+ALLPOS = [(x, y) for x in range(WIDTH) for y in range(HEIGHT)]
 lines = open('day8_input.txt').readlines()
 
-def rect(screen, width, height):
-    ''' turn on pixels in the rectangle defined by a & b '''
-    new_screen = set(screen)
-    for x in range(0, width):
-        for y in range(0, height):
-            new_screen.add((x, y))
-    return new_screen
 
-def rotrow(screen, row, cnt):
-    new_screen = set()
+def make_rect(screen, width, height):
+    rect = set((x, y) for x in range(width) for y in range(height))
+    return rect | screen
 
-    # copy unchanged
-    for y in range(HEIGHT):
-        if y == row:
-            continue
-        for x in range(WIDTH):
-            if (x, y) in screen:
-                new_screen.add((x, y))
+def make_rotrow(screen, row, count):
+    def rotrow_mutator(pos):
+        x, y = pos
+        if y != row:
+            return (x, y)
+        x = (x + count) % WIDTH
+        return (x, y)
+    return set(map(rotrow_mutator, screen))
 
-    for x in range(WIDTH):
-        rotx = (x + cnt) % WIDTH
-        if (x, row) in screen:
-            new_screen.add((rotx, row))
-    return new_screen
-
-def rotcol(screen, row, cnt):
-    new_screen = set()
-
-    # copy unchanged
-    for y in range(HEIGHT):
-        for x in range(WIDTH):
-            if x == row:
-                continue
-            if (x, y) in screen:
-                new_screen.add((x, y))
-
-    for y in range(HEIGHT):
-        roty = (y + cnt) % HEIGHT
-        if (row, y) in screen:
-            new_screen.add((row, roty))
-    return new_screen
+def make_rotcol(screen, col, count):
+    def rotcol_mutator(pos):
+        x, y = pos
+        if x != col:
+            return (x, y)
+        y = (y + count) % HEIGHT
+        return (x, y)
+    return set(map(rotcol_mutator, screen))
 
 def exec_line(screen, line):
-    _rect = (rect, re.compile('rect (\d+)x(\d+)'))
-    _rotrow = (rotrow, re.compile('rotate row y=(\d+) by (\d+)'))
-    _rotcol = (rotcol, re.compile('rotate column x=(\d+) by (\d+)'))
+    _rect = (make_rect, re.compile('rect (\d+)x(\d+)'))
+    _rotrow = (make_rotrow, re.compile('rotate row y=(\d+) by (\d+)'))
+    _rotcol = (make_rotcol, re.compile('rotate column x=(\d+) by (\d+)'))
     parsers = [_rect, _rotrow, _rotcol]
     for func, parse in parsers:
         if parse.match(line):
             args = map(int, parse.match(line).groups())
             return func(screen, *args)
 
-def main():
-    screen = set()
-    for line in lines:
-        screen = exec_line(screen, line)
-    print("CNT", len(screen))
+def pprint_screen(screen):
     for y in range(HEIGHT):
         line = ''
         for x in range(WIDTH):
@@ -75,6 +53,13 @@ def main():
             else:
                 line += ' '
         print(line)
+
+def main():
+    screen = set()
+    for line in lines:
+        screen = exec_line(screen, line)
+    print("CNT", len(screen))
+    pprint_screen(screen)
 
 if __name__ == '__main__':
     main()
